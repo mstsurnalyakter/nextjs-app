7ce4f2fe2ca2376172f2281761f7cc25a5167702
mstsurnalyakter <surnalyms@gmail.com>
2024-10-08 12:40:54 +0600

    add: navigation

--- my-next-app/src/app/about/page.jsx
+++ my-next-app/src/app/about/page.jsx
@@ -1,9 +1,65 @@
+import Link from 'next/link';
 import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
+    <div>
+      <h2 className="text flex items-center text-3xl text-fuchsia-500">
+        AboutPage
+      </h2>
+      <Link href="/about#myName">See my Name</Link>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, odit
+        quam! Expedita veritatis molestias aperiam nostrum, aliquam tempora
+        placeat sit quae quo optio illo minus blanditiis sunt sequi officiis
+        aspernatur quia aut, a iusto dolorum voluptates accusantium est
+        obcaecati ducimus. Qui ut excepturi quisquam repudiandae necessitatibus
+        reprehenderit perferendis laborum dolores, modi illum placeat
+        consequuntur ducimus veniam illo eum nulla possimus omnis. Ipsum
+        suscipit saepe labore excepturi earum soluta quam tenetur maiores sed,
+        recusandae atque corporis dolore enim repellendus quas qui consectetur
+        eveniet neque, quod numquam quidem optio ab! Voluptatum iste ipsa, sint
+        odit deserunt molestias ab unde! Iste sequi optio sit inventore eum
+        ullam, quidem consequuntur et dolore possimus alias nostrum fugit.
+        Numquam et eos debitis voluptates a pariatur porro hic, minima
+        cupiditate eius accusamus facere sed aliquid ad obcaecati nisi dolor
+        impedit magni. Dolor est quam voluptate numquam molestiae modi deleniti
+        consequuntur sit minus eaque rem asperiores fugit iusto animi amet
+        libero perferendis a praesentium, quis ad nisi excepturi tempore eius.
+        Libero assumenda sunt quis iusto ipsam. Quia quos, sint asperiores
+        aliquid dolore ex numquam temporibus. Molestias ea nobis deserunt nihil
+        illo odit natus obcaecati est eos dicta voluptatem distinctio error
+        voluptate beatae, aliquam quam inventore commodi voluptas omnis magnam
+        voluptates sunt fugiat dolore. Illum dignissimos adipisci repellat, cum,
+        iusto fuga provident exercitationem ea rem aliquid impedit! Ad
+        accusantium neque blanditiis sed! Eligendi rerum laudantium et expedita
+        omnis iusto libero ipsam illo? Nemo voluptates vitae molestiae, nobis,
+        similique, sit ut consequuntur dicta adipisci facere quam nulla magni
+        ducimus. Voluptatem officiis molestias quos recusandae repellendus
+        possimus minima, optio cumque, tenetur quae corporis dolores! Mollitia
+        ab vero debitis soluta est. Laboriosam porro doloribus atque minus,
+        perferendis voluptas excepturi cupiditate! Molestias fugit ratione atque
+        rem a accusamus repellendus cupiditate consequuntur aliquid! Vel
+        consequuntur libero placeat quis suscipit natus eveniet facere quibusdam
+        eligendi incidunt alias, voluptatem eum pariatur dolore dolorem
+        cupiditate illo commodi praesentium repudiandae velit ut vitae nobis
+        impedit? Est, veritatis! Ducimus dolor non architecto aut repudiandae
+        quam sed quaerat, qui aliquid iste illum recusandae dignissimos error
+        obcaecati necessitatibus dolore praesentium animi in cum voluptas
+        aliquam ullam. Nihil veritatis beatae libero aliquid quod qui sequi
+        cupiditate inventore recusandae amet, vel magnam atque, dolor
+        asperiores, reiciendis praesentium facilis quasi totam et quis adipisci
+        sapiente debitis ducimus? Neque deserunt, alias, laborum deleniti
+        expedita dolore voluptatum placeat laboriosam optio, doloremque ad
+        magnam nemo assumenda fuga illum cumque aspernatur ullam. Debitis
+        sapiente aliquam voluptatem omnis unde!
+      </p>
+      <h3 id="myName" className="text-red-300 text-4xl text-center">
+        Mst Surnaly Akter
+      </h3>
+    </div>
+  );
 }
 
 export default AboutPage
--- my-next-app/src/components/NavBar.jsx
+++ my-next-app/src/components/NavBar.jsx
@@ -1,16 +1,52 @@
+"use client"
+
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+// import { useRouter } from 'next/router'
 import React from 'react'
 
 const NavBar = () => {
+
+    const pathName = usePathname();
+    const router = useRouter();
+    console.log(pathName);
+
+    const links = [
+        {
+            title:"Home",
+            path:"/"
+        },
+        {
+            title:"About",
+            path:"/about"
+        },
+        {
+            title:"Contact",
+            path:"/contact"
+        },
+        {
+            title:"Services",
+            path:"/services"
+        },
+        {
+            title:"Products",
+            path:"/product"
+        },
+    ]
+
+    const handleClick = () =>{
+        router.push('/about')
+    }
+
   return (
-    <div className='flex bg-green-700 items-center justify-between p-6'>
-        <h6>Logo</h6>
+    <div className='flex bg-purple-400 items-center justify-between p-6'>
+        <h6>Next <span>App</span></h6>
         <ul className='flex items-center justify-center gap-6'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Services</li>
-            <li>Product</li>
+           {
+            links.map(link=><Link className={`${pathName===link.path ? "text-teal-700 underline" : "text-indigo-100"} font-bold text-lg`} href={link.path} key={link.path}>{link.title}</Link>)
+           }
         </ul>
+        <button onClick={handleClick} className='bg-green-600 rounded-md px-3 py-2'>Login</button>
     </div>
   )
 }
